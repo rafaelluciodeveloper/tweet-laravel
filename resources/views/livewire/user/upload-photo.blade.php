7831973023bd99bf573139437c714c6db3b1ad62
Rafael. Lucio <rafaellucio.developer@gmail.com>
2022-03-19 23:04:14 -0300

    finish

--- resources/views/livewire/user/upload-photo.blade.php
+++ resources/views/livewire/user/upload-photo.blade.php
@@ -1,8 +1,15 @@
 <div>
-    <h1>Atualizar Foto Perfil</h1>
-    <form method="POST" action="#"  wire:submit.prevent="storagePhoto">
-        <input type="file" name="photo" id="photo" wire:model="photo">
-        @error('photo'){{ $message }}@enderror
-        <button type="submit" class="btn btn-primary">Upload</button>
+    <h1 class="text-4xl py-6 font-bold">Atualizar Foto do Perfil</h1>
+    <form wire:submit.prevent="storagePhoto" class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-8 m-8">
+        @if ($photo)
+            <div class="mb-4">
+                <img src="{{ $photo->temporaryUrl() }}" class="rounded-full object-cover" style="max-width: 200px;">
+            </div>
+        @endif
+        <div class="mb-4">
+            <input type="file" wire:model="photo">
+        </div>
+        @error('photo') <p><span class="text-red-500">{{ $message }}</span></p> @enderror
+        <button type="submit" class="bg-blue-900 text-white p-2 pl-6 pr-6 rounded">Upload Foto</button>
     </form>
 </div>
